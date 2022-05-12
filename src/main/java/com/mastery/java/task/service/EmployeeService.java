dca7825c39bae96fd91f0868bcdac84d578ccc11
Roman Zubariev <roman.zubariev.1990@gmail.com>
2022-05-12 21:36:40 +0200

    Added spring-boot-starter-web dependency to maven, entry point in EmployeeService.java

--- src/main/java/com/mastery/java/task/service/EmployeeService.java
+++ src/main/java/com/mastery/java/task/service/EmployeeService.java
@@ -1,4 +1,9 @@
 package com.mastery.java.task.service;
-
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
 public class EmployeeService {
+	public static void main (String[] args) {
+		SpringApplication.run(EmployeeService.class, args);
+	}
 }
