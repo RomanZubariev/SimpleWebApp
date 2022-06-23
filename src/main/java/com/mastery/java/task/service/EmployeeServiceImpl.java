8f55d5ace81758cbec922f72ebb77c087fe163fa
Roman Zubariev <roman.zubariev.1990@gmail.com>
2022-06-23 11:13:59 +0200

    Minor formatting

--- src/main/java/com/mastery/java/task/service/EmployeeServiceImpl.java
+++ src/main/java/com/mastery/java/task/service/EmployeeServiceImpl.java
@@ -21,7 +21,8 @@ public class EmployeeServiceImpl implements EmployeeService {
   @Override
   public Employee getById(Long id) {
     return employeeRepository.findById(id).orElseThrow(
-        () -> new EmptyResultDataAccessException("Cannot get employee: id " + id + "not found", 1));
+        () -> new EmptyResultDataAccessException("Cannot get employee: id " + id + " not found",
+            1));
   }
 
   @Override
