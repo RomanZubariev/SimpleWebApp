f5e38479f699e5a58159d115479f3298fff19504
Roman Zubariev <roman.zubariev.1990@gmail.com>
2022-06-23 12:06:29 +0200

    added loggers dependencies to pom

--- src/test/java/com/mastery/java/task/service/EmployeeServiceTest.java
+++ src/test/java/com/mastery/java/task/service/EmployeeServiceTest.java
@@ -1,5 +1,11 @@
 package com.mastery.java.task.service;
 
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@ExtendWith(MockitoExtension.class)
+@SpringBootTest
 public class EmployeeServiceTest {
 
 }
