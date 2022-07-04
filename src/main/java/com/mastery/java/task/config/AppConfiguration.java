4350efa6860ae2a33210d600275ccbe82c45c5c2
Roman Zubariev <roman.zubariev.1990@gmail.com>
2022-07-04 17:23:29 +0200

    Changed title, description and version in openAPI definition.

--- src/main/java/com/mastery/java/task/config/AppConfiguration.java
+++ src/main/java/com/mastery/java/task/config/AppConfiguration.java
@@ -1,8 +1,17 @@
 package com.mastery.java.task.config;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@OpenAPIDefinition(
+    info = @Info(
+      title = "Simple Web App",
+      description = "REST API that performs CRUD operations on Employee objects.",
+      version = "0.0.1-SNAPSHOT"
+    )
+)
 public class AppConfiguration {
 
 }
