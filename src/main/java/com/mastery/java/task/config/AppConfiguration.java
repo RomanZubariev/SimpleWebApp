cc464aae103e18bd57f01b99889dbc3c3c5c6a3c
Roman Zubariev <roman.zubariev.1990@gmail.com>
2022-07-05 09:16:19 +0200

    OpenAPI info (title, description and version) is now taken from pom. Changed the info in pom.
    OpenAPI bean is now redefined explicitly.

--- src/main/java/com/mastery/java/task/config/AppConfiguration.java
+++ src/main/java/com/mastery/java/task/config/AppConfiguration.java
@@ -1,17 +1,24 @@
 package com.mastery.java.task.config;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@OpenAPIDefinition(
-    info = @Info(
-      title = "Simple Web App",
-      description = "REST API that performs CRUD operations on Employee objects.",
-      version = "0.0.1-SNAPSHOT"
-    )
-)
 public class AppConfiguration {
 
+  @Value("${app.name}")
+  private String appName;
+  @Value("${app.version}")
+  private String appVersion;
+  @Value("${app.description}")
+  private String appDescription;
+
+  @Bean
+  public OpenAPI openAPI() {
+    return new OpenAPI().info(
+        new Info().title(appName).description(appDescription).version(appVersion));
+  }
 }
