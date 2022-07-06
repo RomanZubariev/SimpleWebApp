5cb9cc0c8727709cd12fc5e723be66abf3303100
Roman Zubariev <roman.zubariev.1990@gmail.com>
2022-07-06 16:47:47 +0200

    Formatting

--- src/main/java/com/mastery/java/task/dto/EmployeeBirthDateValidator.java
+++ src/main/java/com/mastery/java/task/dto/EmployeeBirthDateValidator.java
@@ -1,11 +1,11 @@
 package com.mastery.java.task.dto;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
-public class EmployeeBirthDateValidator implements ConstraintValidator<EmployeeBirthDate, LocalDate> {
+public class EmployeeBirthDateValidator implements
+    ConstraintValidator<EmployeeBirthDate, LocalDate> {
 
   @Override
   public boolean isValid(LocalDate date, ConstraintValidatorContext context) {
